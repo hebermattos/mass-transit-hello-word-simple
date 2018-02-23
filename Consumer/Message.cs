b73f77dc932c98b5746474628e565ee1f02d8aec
Heber Mattos <hebermattos@outlook.com>
2018-02-22 22:33:22 -0300

    a lot of errors

---
+++ Consumer/Message.cs
@@ -0,0 +1,9 @@
+
+namespace messages
+{
+    public class Message
+    {
+        public string Key { get; set; }
+        public string Value { get; set; }
+    }
+}
--- Consumer/Program.cs
+++ Consumer/Program.cs
@@ -1,12 +1,69 @@
 ﻿using System;
+using System.Threading;
+using messages;
+using MassTransit;
+using StackExchange.Redis;
 
-namespace Consumer
+public class Program
 {
-    class Program
+    private static ManualResetEvent _handler = new ManualResetEvent(false);
+
+    public static void Main()
     {
-        static void Main(string[] args)
+        var connected = false;
+
+        while (!connected)
         {
-            Console.WriteLine("Hello World!");
+            try
+            {
+                ConnectQueue();
+                connected = true;
+                Console.WriteLine("Connected!");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Erro on connection: {ex.Message}");
+            }
         }
+
+        _handler.WaitOne();
+
+    }
+
+    private static void ConnectQueue()
+    {
+        var bus = Bus.Factory.CreateUsingRabbitMq(sbc =>
+        {
+            var host = sbc.Host(new Uri("rabbitmq://queue"), h =>
+            {
+                h.Username("guest");
+                h.Password("guest");
+            });
+
+            sbc.ReceiveEndpoint(host, "message_queue", ep =>
+            {
+                ep.Handler<Message>(context =>
+                {
+                    try
+                    {
+                        using (var redis = ConnectionMultiplexer.Connect("redis"))
+                        {
+                            IDatabase db = redis.GetDatabase();
+
+                            if (!db.StringSet(context.Message.Key, context.Message.Value))
+                                throw new Exception("Message not saved on redis :(");
+                        }
+
+                        return Console.Out.WriteLineAsync("Message processed");
+                    }
+                    catch (Exception ex)
+                    {
+                        return Console.Out.WriteLineAsync($"Erro on message processing: {ex.Message}");
+                    }
+                });
+            });
+        });
+
+        bus.Start();
     }
 }
