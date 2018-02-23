6330ec2b831136afcf05136a091ad7f4c9c34834
Heber Mattos <hebermattos@outlook.com>
2018-02-22 22:09:33 -0300

    new receiver

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace mass_transit_hello_word
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
